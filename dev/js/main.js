f2340d27dd7a4ff92c9972efdf0b2173cba8fc80
Alex Syrtsov <alexander.publicity@gmail.com>
2016-03-10 19:20:14 +0300

    Изменение прозрачности

---
+++ dev/js/main.js
@@ -0,0 +1,122 @@
+// Стрелочки для инпутов
+$(function() {
+  $( "#spinnerX" ).spinner({
+    min:0
+  });
+  $( "#spinnerY" ).spinner({
+    min:0
+  });
+  $( "#spinnerVert" ).spinner({
+    min:0
+  });
+  $( "#spinnerHor" ).spinner({
+    min:0
+  });
+});
+// Слайдер
+$(function() {
+  $( "#slider" ).slider({
+      range: "min",
+      min: 1,
+      max: 100,
+      value:50,
+                //Получаем значение и выводим его на странице
+                slide: function( event, ui ) {
+                      $( ".slider-result" ).html( ui.value );
+                },
+
+                //Обновляем скрытое поле формы, так что можно передать данные с помощью формы
+                change: function(event, ui) { 
+                  $('.hidden').attr('value', ui.value);
+                  console.log(ui.value);
+                var opacityValue = ui.value * 0.01;
+                  $('.loaded__watermark').css('opacity', opacityValue);
+                  console.log(opacityValue);
+                }
+  });
+});
+// Код для скрытых инпутов типа файл, и текстовых инпутов
+ if($('.download-background')){
+    _chooseFileBackground()
+ }
+  if($('.download-watermark')){
+    _chooseFileWatermark()
+ }
+ function _chooseFileBackground(){
+
+            $('.download-background').click(function(){
+                $('.download-background__file').click();
+                return(false);
+            });
+            $('.download-background__file').on('change',function(){
+                //console.log($('.file-upload').val())
+                $('.download-background__text').val($('.download-background__file').val());
+                var text= $('.download-background__text').val();
+                text = text.replace("C:\\fakepath\\", "");
+                $('.download-background__text').val(text);
+                $('.loaded__image').load(function() {
+                    $('.loaded__image').position({
+                          of: $('.canvas-content__wrapper'),
+                          my: 'center center',
+                          at: 'center center',
+                      });
+                });
+            }).change();// .change() в конце для того чтобы событие сработало при обновлении страницы
+
+ };
+  function _chooseFileWatermark(){
+
+            $('.download-watermark').click(function(){
+                $('.download-watermark__file').click();
+                return(false);
+            });
+            $('.download-watermark__file').on('change',function(){
+                // console.log($('.file-upload').val())
+                $('.download-watermark__text').val($('.download-watermark__file').val());
+                var text= $('.download-watermark__text').val();
+                text = text.replace("C:\\fakepath\\", "");
+                $('.download-watermark__text').val(text);
+                imageHandling.imageLoad();
+            }).change();// .change() в конце для того чтобы событие сработало при обновлении страницы
+
+ };
+
+// Переключатель
+ $('.switch__link').on('click', _changeView);
+ function _changeView(e){
+    e.preventDefault();
+    _change($(this));
+ }
+ previosClass = '';
+function _change($this){
+
+  item = $this.closest('.switch__link'),
+  itemClass = item.attr('class'),
+  view = item.attr("data-view"),
+  views = $('#views'),
+  prefix = 'position_',
+  classOfViewState = prefix + view; 
+  if (previosClass == '') {
+    previosClass = views.attr('class');
+  } views.attr('class', previosClass+ ' ' +classOfViewState);
+} 
+
+  switchTile = $('.switch__link_tile');
+  switchSingle = $('.switch__link_single');
+  switchTileActive = 'switch__link_tile_active';
+  switchSingleActive = 'switch__link_single_active';
+
+  switchTile.on('click', function(event) {
+    event.preventDefault();
+    if (!switchTile.hasClass(switchTileActive)) {
+      switchTile.addClass(switchTileActive);
+      switchSingle.removeClass(switchSingleActive);
+    }
+  });
+  switchSingle.on('click', function(event) {
+    event.preventDefault();
+    if (!switchSingle.hasClass(switchSingleActive)) {
+      switchSingle.addClass(switchSingleActive);
+      switchTile.removeClass(switchTileActive);
+    }
+  });
